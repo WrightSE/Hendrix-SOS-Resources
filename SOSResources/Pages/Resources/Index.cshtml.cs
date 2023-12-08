46401de5e10bb375fa3abfd4c8adf64b955efd7c
Amelia Jones <jonesas@hendrix.edu>
2023-12-08 14:04:26 -0600

    Resources can be sorted by name or type

--- SOSResources/Pages/Resources/Index.cshtml.cs
+++ SOSResources/Pages/Resources/Index.cshtml.cs
@@ -18,15 +18,41 @@ namespace SOSResources.Pages.Resources
         {
             _context = context;
         }
+        public string NameSort { get; set; }
+        public string TypeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public IList<Resource> Resource { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        
+        public IList<Resource> Resources { get;set; } = default!;
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Resources != null)
-            {
-                Resource = await _context.Resources.ToListAsync();
+            
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            TypeSort = sortOrder == "Type" ? "type_desc" : "Type";
+
+            IQueryable<Resource> resourcesIQ = from r in _context.Resources
+                                                    select r;
+            switch(sortOrder){
+                case"name_desc":
+                    resourcesIQ = resourcesIQ.OrderByDescending(r => r.Name);
+                    break;
+                case"Type":
+                resourcesIQ = resourcesIQ.OrderBy(r => r.Type);
+                break;
+                case"type_desc":
+                    resourcesIQ = resourcesIQ.OrderByDescending(r => r.Type);
+                    break;
+                default:
+                    resourcesIQ = resourcesIQ.OrderBy(r => r.Name);
+                    break;
             }
+            
+
+                Resources = await resourcesIQ.AsNoTracking().ToListAsync();
+            
         }
     }
 }
